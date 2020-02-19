4f41772290af80abdd5ee20d989cb3cc82ce9a29
Oleksandr Kulinich <okulinich@e2r7p9.unit.ua>
2020-02-19 17:48:41 +0200

    basic structure

--- inc/ush.h
+++ inc/ush.h
@@ -1,8 +1,18 @@
 #ifndef USH_H
 #define USH_H
 
-#include "./libmx/inc/libmx.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+#define BUFSIZE 1024
+#define DELIMITERS "\t\r\n\a "
 
+typedef struct s_lst {
+    char *cmd; //"ls"
+    char **av; //{"-la", "src"}
+    struct s_lst *next;
+} t_lst;
 
 #endif
