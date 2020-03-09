fd0e9bcf413f58902f337cd6bf58fa4431c6e3ba
Andrii Ilchuk <ailchuk@e2r5p2.unit.ua>
2020-03-09 17:42:41 +0200

    done cd and pwd

--- src/built_pwd.c
+++ src/built_pwd.c
@@ -1,64 +1,66 @@
 #include "ush.h"
 
-// bool is_flag_stop(char *flag) {
-//     if ((flag[1] == '-' && flag[2] == '\0')
-//         || flag[1] == '\0') {
-//         return true;
-//     }
-//     return false;
-// }
-
-// static bool parse_flags(char **flags, bool *mode) {
-//     bool flag_stop = 0;
-
-//     for (int i = 1; flags[i]; i++) {
-//         if (flags[i][0] == '-' && !flag_stop) {
-//             flag_stop = is_flag_stop(flags[i]);
-//             for (int j = 2; flags[i][j] != '\0'; j++)
-//                 if (flags[i][j] == 'P')
-//                     *mode = 1;
-//                 else if (flags[i][j] != 'L' && (flags[i][j] != '-'
-//                          || (flags[i][j] == '-' && j == 2))) {
-//                     fprintf(stderr, "pwd: bad option: -%c\n", flags[i][j]);
-//                     return 1;
-//                 }
-//         }
-//         else {
-//             fprintf(stderr, "pwd: too many arguments\n");
-//             return 1;
-//         }
-//     }
-//     return 0;
-// }
-
-int mx_builtin_pwd(t_global *hd, t_lst *head) { // сделать usage!!  переделать парсер фалагов !!!!!!!! (примеры pwd w, pwd -qwe, pwd 123, pwd 213)
-    // bool flag = 0;
-    char *pwd = mx_get_env_var(hd->env, "PWD");
-    // char *tmp = NULL;
-
-    // if (!parse_flags(hd->new->av, &flag)) {
-    if (head->av[1] == NULL) {
-        printf("NO FLAGS:%s\n", pwd);
+static int one_more_cycle(char **av, int i, int *p) {
+    for (int j = 1; av[i][j]; j++) {
+        if (av[i][j] == 'P')
+            *p = 1;
+        else if (av[i][j] != 'L' && 
+                (av[i][j] != '-' || (av[i][j] == '-' && j == 2))) {
+            fprintf(stderr, "pwd: bad option: -%c\n", av[i][j]);
+            return -1;
+        }
     }
-    if (mx_strcmp(head->av[1], "-P") == 0) {
-        printf("IF P:%s\n", pwd); // logical -> use PWD from environment, even if it contains symlinks
-    }
-    else if (mx_strcmp(head->av[1], "-L") == 0) {
-        printf("IF L:%s\n", pwd);
-        mx_strdel(&pwd);
-    }
-    else {
-        printf("usage: pwd [-L | -P]\n");
-        // mx_strdel(&pwd);
-        mx_strdel(&head->cmd);
-        mx_del_strarr(&head->av);
+    return 0;
+}
 
+static int get_flags(char **av) {
+    bool stop = 0;
+    int p = 0;
+
+    for (int i = 1; av[i] != NULL; i++) {
+        if (av[i][0] == '-' && !stop) {
+            if ((av[i][1] == '-' && av[i][2] == '\0') || av[i][1] == '\0')
+                stop = 1;
+            if (one_more_cycle(av, i, &p) == -1)
+                return -1;
+        }
+        else {
+            fprintf(stderr, "pwd: too many arguments\n");
+            return -1;
+        }
     }
-    // if (strcmp(hd->new->av[1], "P")) {
-    //     tmp = realpath(pwd, NULL);
-    //     printf("IF:%s\n", tmp);
-    //     mx_strdel(&tmp);
-    //     }
-    printf("\n\t\tbuiltin pwd\n");
+    return p;
+}
+
+static void pwd_default(void) {
+    char *res = strdup(getenv("PWD"));
+
+    printf("%s\n", res);
+    mx_strdel(&res);
+}
+
+static void pwd_flagp(void) {
+    char *path = strdup(getenv("PWD"));
+    char realbuf[PATH_MAX];
+
+    if (mx_islink(path)) {
+        char *res = realpath(path, realbuf);
+        if (res)
+            printf("%s\n", realbuf);
+    }
+    else
+        printf("%s\n", path);
+    mx_strdel(&path);
+}
+
+int mx_builtin_pwd(t_lst *head) { 
+    int p;
+
+    if ((p = get_flags(head->av)) != -1) {
+        if (p)
+            pwd_flagp();
+        else
+            pwd_default();
+    }
     return 1;
 }
