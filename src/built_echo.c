11715d747b93f1d72706422f0179087955fd9222
Oleksandr Kulinich <okulinich@e2r7p9.unit.ua>
2020-03-11 14:50:46 +0200

    echo with all flags realized

--- src/built_echo.c
+++ src/built_echo.c
@@ -14,19 +14,118 @@ static char *search_for_var(t_global *hd, char *str) {
     return NULL;
 }
 
+// \' одинарная кавычка
+// \" двойная кавычка
+// \? вопросительный знак
+// \\ обратный слеш
+// \0 нулевой символ
+// \a звуковой сигнал
+// \b забой
+// \f перевод страницы - новая страница
+// \n перевод строки - новая строка
+// \r возврат каретки
+// \t горизонтальная табуляция
+// \v вертикальная табуляция
+
+static void print_with_escape(char *str) {
+    for(int i = 0; i < mx_strlen(str); i++) {
+        if(str[i] == '\\' && str[i + 1]) {
+            switch(str[i + 1]) {
+                case '\'':
+                    mx_printchar('\'');
+                    i++;
+                    break;
+                case '\"':
+                    mx_printchar('\"');
+                    i++;
+                    break;
+                case '?':
+                    mx_printchar('\?');
+                    i++;
+                    break;
+                case '\\':
+                    mx_printchar('\\');
+                    i++;
+                    break;
+                case '0':
+                    mx_printchar('\0');
+                    i++;
+                    break;
+                case 'a':
+                    mx_printchar('\a');
+                    i++;
+                    break;
+                case 'b':
+                    mx_printchar('\b');
+                    i++;
+                    break;
+                case 'f':
+                    mx_printchar('\f');
+                    i++;
+                    break;
+                case 'n':
+                    mx_printchar('\n');
+                    i++;
+                    break;
+                case 'r':
+                    mx_printchar('\r');
+                    i++;
+                    break;
+                case 't':
+                    mx_printchar('\t');
+                    i++;
+                    break;
+                case 'v':
+                    mx_printchar('\v');
+                    i++;
+                    break;
+            }
+        }
+        else
+            mx_printchar(str[i]);
+    }
+}
+
+static void parse_flags(bool *no_new_line, bool *escape_off, t_lst *head, int *i) {
+    bool flag = true;
+
+    for(int j = 1; head->av[j]; j++) {
+        if (head->av[j][0] == '-' && flag) {
+            if (mx_strcmp(head->av[j], "-n") == 0 || mx_strcmp(head->av[j], "-enE") == 0 || mx_strcmp(head->av[j], "-Ene") == 0 || 
+                mx_strcmp(head->av[j], "-nEe") == 0 || mx_strcmp(head->av[j], "-neE") == 0 ||
+                mx_strcmp(head->av[j], "-Een") == 0 || mx_strcmp(head->av[j], "-eEn") == 0 || mx_strcmp(head->av[j], "-en") == 0) {
+                *no_new_line = true;
+                (*i)++;
+            }
+            else if(mx_strcmp(head->av[j], "-E") == 0) {
+                *escape_off = true;
+                (*i)++;
+            }
+            else if(mx_strcmp(head->av[j], "-e") == 0 || mx_strcmp(head->av[j], "-eE") == 0 || mx_strcmp(head->av[j], "-Ee") == 0) {
+                (*i)++;
+                continue;
+            }
+            else if(mx_strcmp(head->av[j], "-nE") == 0 || mx_strcmp(head->av[j], "-En") == 0) {
+                *no_new_line = true;
+                *escape_off = true;
+                (*i)++;
+            }
+        }
+        else
+            flag = false;
+    }
+}
 
 int mx_echo(t_global *hd, t_lst *head) {
     char *str;
     bool no_new_line = false;
+    bool escape_off = false;
     int i = 1;
 
     if(!head->av[1])
         mx_printstr("\n");
     else {
-        if(mx_strcmp(head->av[1], "-n") == 0) {
-            no_new_line = true;
-            i++;
-        }
+        parse_flags(&no_new_line, &escape_off, head, &i);
         for(; head->av[i]; i++) {
             if(head->av[i][0] == '$') {
                 str = search_for_var(hd, head->av[i]);
@@ -35,7 +134,7 @@ int mx_echo(t_global *hd, t_lst *head) {
                 }
             }
             else {
-                mx_printstr(head->av[i]);
+                escape_off == true ? mx_printstr(head->av[i]) : print_with_escape(head->av[i]);
             }
             if(head->av[i + 1])
                 mx_printstr(" ");
